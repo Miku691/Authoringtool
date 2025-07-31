66ca6a3957af0d40dc4d420cd48652ec14eb032a
Makaradhwaja Behera-Nichebit <makaradhwaja@cholamsispl.com>
2025-07-31 12:49:03 +0530

    project set up

---
+++ src/main/java/com/authoring/tool/AuthoringtoolApplication.java
@@ -0,0 +1,13 @@
+package com.authoring.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoringtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoringtoolApplication.class, args);
+	}
+
+}
