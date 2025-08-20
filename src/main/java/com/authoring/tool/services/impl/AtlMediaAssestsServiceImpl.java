377a716db20b1905caba32ca9e484324eb2d943f
Makaradhwaja Behera-Nichebit <makaradhwaja@cholamsispl.com>
2025-08-20 17:39:15 +0530

    add test class

--- src/main/java/com/authoring/tool/services/impl/AtlMediaAssestsServiceImpl.java
+++ src/main/java/com/authoring/tool/services/impl/AtlMediaAssestsServiceImpl.java
@@ -5,22 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.authoring.tool.dto.AtlMediaAssestsDto;
+import com.authoring.tool.entity.AtlComponentTracking;
 import com.authoring.tool.entity.AtlMediaAssests;
 import com.authoring.tool.exception.DetailsNotFoundException;
+import com.authoring.tool.repo.AtlComponentTrackingRepo;
 import com.authoring.tool.repo.AtlMediaAssestsRepo;
 import com.authoring.tool.services.AtlMediaAssestsService;
+import com.authoring.tool.utility.ApplicationConstant;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class AtlMediaAssestsServiceImpl implements AtlMediaAssestsService{
 
-	@Autowired
-	private ModelMapper modelMapper;
-	@Autowired
-	private AtlMediaAssestsRepo assestsRepo;
+	private final ModelMapper modelMapper;
+	private final AtlMediaAssestsRepo assestsRepo;
+	private final AtlComponentTrackingRepo trackingRepo;
 	
 	@Override
-	public AtlMediaAssestsDto saveMediaAssest(AtlMediaAssestsDto mediaAsset) {
+	@Transactional
+	public AtlMediaAssestsDto saveMediaAssest(AtlMediaAssestsDto mediaAsset, Long slideId) {
+		
 		AtlMediaAssests mediaAssetSaved = assestsRepo.save(modelMapper.map(mediaAsset, AtlMediaAssests.class));
+		trackingRepo.save(new AtlComponentTracking(null, slideId,
+				mediaAssetSaved.getId(),
+				ApplicationConstant.ATL_MEDIA_ASSEST));
 		return modelMapper.map(mediaAssetSaved, AtlMediaAssestsDto.class);
 	}
 
